8310f09e50f1c10da130f7d530eec9f7024b35ed
alsxm4625@naver.com <129379922+alsxm4625@users.noreply.github.com>
2023-11-07 21:53:30 +0900

    첫번째 commit

---
+++ app/src/main/java/com/example/sogating/auth/IntroActivity.kt
@@ -0,0 +1,55 @@
+package com.example.sogating.auth
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.sogating.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        val isTiramisuOrHigher = Build.VERSION.SDK_INT>= Build.VERSION_CODES.TIRAMISU
+        val notificationPermission = Manifest.permission.POST_NOTIFICATIONS
+
+        var hasNotificationPermission =
+            if (isTiramisuOrHigher)
+                ContextCompat.checkSelfPermission(this, notificationPermission) == PackageManager.PERMISSION_GRANTED
+            else true
+
+        val launcher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            hasNotificationPermission = it
+
+        }
+
+        if (!hasNotificationPermission) {
+            launcher.launch(notificationPermission)
+        }
+
+        val loginBtn : Button = findViewById(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        val joinBtn : Button = findViewById(R.id.joinBtn)
+        joinBtn.setOnClickListener {
+
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
